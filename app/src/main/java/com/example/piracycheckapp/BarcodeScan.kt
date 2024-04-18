d225c33d4e93e0a8499f5fe4819f7c6385b3108f
Kunalsharmassccglpinnacle <kunalsharma@ssccglpinnacle.com>
2024-04-18 15:33:32 +0530

    post api responce

--- app/src/main/java/com/example/piracycheckapp/BarcodeScan.kt
+++ app/src/main/java/com/example/piracycheckapp/BarcodeScan.kt
@@ -13,7 +13,7 @@ import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
 
 import okhttp3.*
-import org.json.JSONArray
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import org.json.JSONObject
 import java.io.IOException
 
@@ -81,11 +81,52 @@ open class BarcodeScan : AppCompatActivity() {
             }
 
 
+//            private fun checkBarcodeInDatabase(barcode: String) {
+//                val client = OkHttpClient()
+//
+//                val request = Request.Builder()
+//                    .url("https://nodei.ssccglpinnacle.com/getScannedInbound")
+//                    .build()
+//
+//                client.newCall(request).enqueue(object : Callback {
+//                    override fun onFailure(call: Call, e: IOException) {
+//                        e.printStackTrace()
+//                        // Handle failure
+//                        runOnUiThread {
+//                            Toast.makeText(applicationContext, "Failed to connect to server", Toast.LENGTH_SHORT).show()
+//                        }
+//                    }
+//
+//                    override fun onResponse(call: Call, response: Response) {
+//                        response.use {
+//                            if (!response.isSuccessful) {
+//                                // Handle unsuccessful response
+//                                runOnUiThread {
+//                                    Toast.makeText(applicationContext, "Failed to get response from server", Toast.LENGTH_SHORT).show()
+//                                }
+//                            } else {
+//                                val responseData = response.body?.string()
+//                                responseData?.let { parseApiResponse(it, barcode) }
+//                            }
+//                        }
+//                    }
+//                })
+//            }
+
+//try
             private fun checkBarcodeInDatabase(barcode: String) {
                 val client = OkHttpClient()
 
+                val json = JSONObject()
+                json.put("barcode", barcode)
+
+                val requestBody = json.toString()
+
                 val request = Request.Builder()
-                    .url("https://nodei.ssccglpinnacle.com/getScannedInbound")
+                    .url("https://nodei.ssccglpinnacle.com/searchBarr1")
+
+                   // .url("https://nodei.ssccglpinnacle.com/count")
+                    .post(RequestBody.create("application/json".toMediaTypeOrNull(), requestBody))
                     .build()
 
                 client.newCall(request).enqueue(object : Callback {
@@ -113,44 +154,88 @@ open class BarcodeScan : AppCompatActivity() {
                 })
             }
 
-            private fun parseApiResponse(responseData: String, scannedBarcode: String) {
-                val jsonArray = JSONArray(responseData)
-                var found = false
-                var message = ""
-
-                for (i in 0 until jsonArray.length()) {
-                    val jsonObject = jsonArray.getJSONObject(i)
-                    val barcodeData = jsonObject.getJSONArray("barcodeData")
-
-                    for (j in 0 until barcodeData.length()) {
-                        if (barcodeData.getString(j) == scannedBarcode) {
-                            found = true
-                            val title = jsonObject.getString("Title")
-                            val orderno = jsonObject.getString("orderno")
-                            val BatchID = jsonObject.getString("BatchID")
-                            val date = jsonObject.getString("date")
-
-                            //message = "Scanned barcode belongs to Pinnacle Data base so book is original with: Tittle-->$title, Order no --> $orderno,Batch ID --> $BatchID and Date-->$date"
-                            message = "Scanned barcode belongs to Pinnacle Data base so book is original with:\nTitle-->$title\nOrder no --> $orderno\nBatch ID --> $BatchID\nDate-->$date"
 
 
-                            break
-                        }
-                    }
-                    if (found) {
-                        break
-                    }
-                }
 
-                if (!found) {
-                    message = "Scanned barcode does not belong to the database."
-                }
+           // try
+//            private fun parseApiResponse(responseData: String, barcode: String) {
+//                try {
+//                    val jsonObject = JSONObject(responseData)
+//                    val found = jsonObject.getBoolean("found")
+//                    var message = ""
+//
+//                    if (found) {
+//
+////                        val responseData = jsonObject.getString("responseData")
+////                        val barcode = jsonObject.getString("barcode")
+//                        val result = jsonObject.getString("result")
+//                        message = "Scanned barcode belongs to Pinnacle Data base with value: $result "
+//                    } else {
+//                        message = "Scanned barcode does not belong to the database."
+//                    }
+//
+//                    runOnUiThread {
+//                        binding.txtMessage.text = message
+//                        binding.txtMessage.visibility = View.VISIBLE
+//                    }
+//                } catch (e: Exception) {
+//                    e.printStackTrace()
+//                    runOnUiThread {
+//                        Toast.makeText(applicationContext, "Failed to parse response", Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//            }
+//
 
+
+            private fun parseApiResponse(responseData: String, barcode: String) {
                 runOnUiThread {
-                    binding.txtMessage.text = message
+                    binding.txtMessage.text = responseData
                     binding.txtMessage.visibility = View.VISIBLE
                 }
             }
+
+
+
+
+//            private fun parseApiResponse(responseData: String, scannedBarcode: String) {
+//                val jsonArray = JSONArray(responseData)
+//                var found = false
+//                var message = ""
+//
+//                for (i in 0 until jsonArray.length()) {
+//                    val jsonObject = jsonArray.getJSONObject(i)
+//                    val barcodeData = jsonObject.getJSONArray("barcodeData")
+//
+//                    for (j in 0 until barcodeData.length()) {
+//                        if (barcodeData.getString(j) == scannedBarcode) {
+//                            found = true
+//                            val title = jsonObject.getString("Title")
+//                            val orderno = jsonObject.getString("orderno")
+//                            val BatchID = jsonObject.getString("BatchID")
+//                            val date = jsonObject.getString("date")
+//
+//                            //message = "Scanned barcode belongs to Pinnacle Data base so book is original with: Tittle-->$title, Order no --> $orderno,Batch ID --> $BatchID and Date-->$date"
+//                            message = "Scanned barcode belongs to Pinnacle Data base so book is original with:\nTitle-->$title\nOrder no --> $orderno\nBatch ID --> $BatchID\nDate-->$date"
+//
+//
+//                            break
+//                        }
+//                    }
+//                    if (found) {
+//                        break
+//                    }
+//                }
+//
+//                if (!found) {
+//                    message = "Scanned barcode does not belong to the database."
+//                }
+//
+//                runOnUiThread {
+//                    binding.txtMessage.text = message
+//                    binding.txtMessage.visibility = View.VISIBLE
+//                }
+//            }
 
             // Modify your receiveDetections function to call checkBarcodeInDatabase
             override fun receiveDetections(p0: Detector.Detections<Barcode>) {
